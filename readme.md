1603846a1a21ecf77c4d8e69f333efebb524490b
simi0973 <simi0973.be23@chitkara.edu.in>
2026-05-13 22:52:57 +0530

    email reminder added

---
+++ readme.md
@@ -0,0 +1,7 @@
+clerk provide  user data, to get that user data from clerk we use clerk web hooks.
+
+to efficiently manage clerk web hooks scheduling & chron jobs we use inngest(provides multiple features to manage batch processing,queuing,background jobs,scheduling jobs,chron jobs)
+
+gitignore is for  these files are not uploading in github
+
+To upload images online we use imagekit(it is an image and video API. using this we can upload and optimize our images   )
--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
+import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
-import dotenv from 'dotenv';
-dotenv.config();
+
 import connectDB from './configs/db.js';
 import {inngest, functions} from './inngest/index.js'
 import {serve} from 'inngest/express';
+import { clerkMiddleware } from '@clerk/express';
+import userRouter from './routes/userRoutes.js';
 
 const app=express();
 
@@ -14,12 +16,24 @@ await connectDB();
 //add middleware 
 app.use(express.json());
 app.use(cors()); 
+app.use(clerkMiddleware());
 
 //creating routes
 app.get('/',(req,res)=>res.send('Server is running'))
 app.use('/api/inngest', serve({ client: inngest, functions }))
+app.use('/api/user', userRouter)
 
 //to star app adding port
 const PORT=process.env.PORT || 4000;
 
 app.listen(PORT, ()=>console.log(`Server is running on port ${PORT}`))
+
+
+
+
+
+
+
+
+
+
