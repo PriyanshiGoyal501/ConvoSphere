cc872b3263cc2c556f110f30f4255f4a674df9b9
simi0973 <simi0973.be23@chitkara.edu.in>
2026-05-14 23:11:35 +0530

    Backend Complete

--- server/server.js
+++ server/server.js
@@ -7,6 +7,8 @@ import {inngest, functions} from './inngest/index.js'
 import {serve} from 'inngest/express';
 import { clerkMiddleware } from '@clerk/express';
 import userRouter from './routes/userRoutes.js';
+import postRouter from './routes/postRoutes.js';
+import storyRouter from './routes/storyRoutes.js';
 
 const app=express();
 
@@ -22,6 +24,9 @@ app.use(clerkMiddleware());
 app.get('/',(req,res)=>res.send('Server is running'))
 app.use('/api/inngest', serve({ client: inngest, functions }))
 app.use('/api/user', userRouter)
+app.use('/api/post', postRouter)
+app.use('/api/story', storyRouter)
+
 
 //to star app adding port
 const PORT=process.env.PORT || 4000;
